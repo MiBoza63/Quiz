36ee6059c8059bcb35745fd24f3e25b8a592a015
Michael Boza <mboza6362@gmail.com>
2026-08-21 22:07:33 -0600

    Added comments in case of test failure

--- test_main.cpp
+++ test_main.cpp
@@ -1,11 +1,21 @@
 #include <gtest/gtest.h>
 
+struct Foo{
+    int a = 2;
+    int b = 4;
+};
+
 TEST(SanityCheck, BasicMath) {
-    
-    EXPECT_EQ(2 + 2, 4);
-    EXPECT_TRUE(false);
+    EXPECT_EQ(2 + 2, 5) << "Basic addition is broken";
+    ASSERT_EQ(1, 12) << "This checks if one equals two — expected to fail";
 }
+
+// TEST(SanityCheck, BasicMath) {
+//     Foo bar;
+//     EXPECT_EQ(2 + 2, 4);    // Numerical Test
+//     EXPECT_EQ(bar.a, 6);    // Object test
+// }
 
 TEST(SanityCheck, StringCompare) {
     EXPECT_STREQ("hello", "hello");
-}   
+}
